afc66eb376ea47ff1b61d6ee3f19d2ae0c327108
ISC Tesoem <tesoem@iMac-Tesoem-01.local>
2019-10-03 15:38:56 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/kjrm/p3kjrm/MainActivity.java
@@ -0,0 +1,26 @@
+package mx.edu.tesoem.isc.kjrm.p3kjrm;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent principal=new Intent(MainActivity.this,FrmActivityPrincipal.class);
+                startActivity(principal);
+                finish();
+
+            }
+        },5000);
+    }
+}
